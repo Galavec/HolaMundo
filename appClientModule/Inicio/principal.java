bd690fbf828b7b909a532f14e7947540c2ba0023
Galavec <galavec@hotmail.com>
2020-11-04 13:40:28 -0500

    Version 1.0

---
+++ appClientModule/Inicio/principal.java
@@ -0,0 +1,15 @@
+package Inicio;
+
+
+public class principal {
+	public static void main(String[] args) {
+
+		System.out.println("Hola Mundo");
+
+		String[] phoneln = {"1","2","3"};
+
+		for (int i = 0; i < phoneln.length; i++) {
+			System.out.println("hola: " + i);
+		}
+	}
+}
